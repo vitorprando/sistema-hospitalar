1803fdbcbbc1bb051fee8a0f09e20b6141542990
Vitor P <100450996+vitorprando@users.noreply.github.com>
2023-12-01 23:52:30 -0300

    e

--- mobileDART/MenuPrincipal.dart
+++ mobileDART/MenuPrincipal.dart
@@ -19,31 +19,99 @@ class MyApp extends StatelessWidget {
             leading: Icon(Icons.medical_services),
             title: Text('Receitas médicas e Controle de Receitas'),
             onTap: () {
-              // Navegue para a tela de Receitas médicas e Controle de Receitas
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ReceitasPage()),
+              );
             },
           ),
           ListTile(
             leading: Icon(Icons.article),
             title: Text('Atestados Médicos'),
             onTap: () {
-              // Navegue para a tela de Atestados Médicos
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AtestadosPage()),
+              );
             },
           ),
           ListTile(
             leading: Icon(Icons.people),
             title: Text('Cadastro de Pacientes e Médicos'),
             onTap: () {
-              // Navegue para a tela de Cadastro de Pacientes e Médicos
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CadastroPage()),
+              );
             },
           ),
           ListTile(
             leading: Icon(Icons.schedule),
             title: Text('Agendamento de consultas'),
             onTap: () {
-              // Navegue para a tela de Agendamento de consultas
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AgendamentoPage()),
+              );
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ReceitasPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Receitas médicas e Controle de Receitas'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da página de Receitas'),
+      ),
+    );
+  }
+}
+
+class AtestadosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Atestados Médicos'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da página de Atestados'),
+      ),
+    );
+  }
+}
+
+class CadastroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Pacientes e Médicos'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da página de Cadastro'),
+      ),
+    );
+  }
+}
+
+class AgendamentoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agendamento de consultas'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da página de Agendamento'),
       ),
     );
   }
